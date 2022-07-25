30f9baf097a3b46da85f5ee60242c6c6d15ae68c
carmelo <carmeload@gmail.com>
2022-07-25 18:29:53 +0200

    custom my menu togles

--- header.php
+++ header.php
@@ -24,32 +24,25 @@
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<header id="masthead" class="site-header">
-		<nav class="navbar navbar-expand-md bg-light border-bottom border-primary border-5 fixed-top">
-    		<div class="container">
+        <div class="site-branding">
 				<?php 
                 the_custom_logo(); 
 			    $gsmtc_description = get_bloginfo( 'description', 'display' );
 			    if ( $gsmtc_description || is_customize_preview() ) :
 				// To take efect de enable/disable of description must use class "site-description"
                 ?>
-				<span class="site-description text-primary"><?php echo $gsmtc_description;  ?></span>
+				<p class="site-description"><?php echo $gsmtc_description;  // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			    <?php endif; ?>                
-				<button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu-1" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-            	</button>
-                <div class="collapse navbar-collapse primary" id="main-menu">
-                <?php
-                wp_nav_menu(array(
-                    'theme_location' => 'main-menu',
-                    'container' => false,
-                    'menu_class' => '',
-                    'fallback_cb' => '__return_false',
-                    'items_wrap' => '<ul id="%1$s" class="navbar-nav ms-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
-                    'depth' => 2,
-                    'walker' => new bootstrap_5_wp_nav_menu_walker()
-                ));
-                ?>
-                </div>
-            </div>
-        </nav>
+        </div> <!-- .site-branding -->
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"></button>
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
