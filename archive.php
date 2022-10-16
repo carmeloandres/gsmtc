7748e251266414fa3bb50af268b2dfaa9ce848bb
carmelo <carmeload@gmail.com>
2022-10-16 08:35:19 +0200

    Archive file ready

--- archive.php
+++ archive.php
@@ -9,30 +9,57 @@
 
 get_header();
 ?>
-	<main id="primary" class="site-main">
-
-<?php if ( have_posts() ) : ?>
-
-	<header class="page-header">
-		<?php
-		the_archive_title( '<h1 class="page-title">', '</h1>' );
-		the_archive_description( '<div class="archive-description">', '</div>' );
-		?>
-	</header><!-- .page-header -->
-
-	<?php
-	/* Start the Loop */
-	while ( have_posts() ) :
-		the_post();
-
-		/*
-		 * Include the Post-Type-specific template for the content.
-		 * If you want to override this in a child theme, then include a file
-		 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-		 */
-		get_template_part( 'template-parts/content', get_post_type() );
-
-	endwhile;
+<div id="content" class="container">
+	<div class="row">
+		<div class="col">
+			<main id="primary" class="site-main">
+	
+			<header class="page-header">
+					<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+			
+			<?php if ( have_posts() ) : ?>
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post(); ?>
+			<div class="card horizontal mb-4">
+				<div class="row">
+					<?php if (has_post_thumbnail())
+						echo '<div class="card-img-left-md col-lg-5">' . get_the_post_thumbnail(null, 'medium') . '</div>';
+					?>
+					<div class="col">
+						<div class="card-body">
+							<?php gsmtc_category_badge(); ?>
+                             <!-- Title -->
+                            <h2 class="blog-post-title">
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h2>
+							<?php if ( 'post' === get_post_type() ) : ?>
+                                <small class="text-muted mb-2">
+                            <?php
+									gsmtc_date();
+									gsmtc_author();
+									gsmtc_comments();
+									gsmtc_edit();
+									?>
+                                    </small>
+                            <?php endif; ?>
+                            <!-- Excerpt & Read more -->
+                            <div class="card-text mt-auto">
+                                <?php the_excerpt(); ?> <a class="read-more" href="<?php the_permalink(); ?>"><?php _e('Leer más »', 'gsmtc'); ?></a>
+                        	</div>
+                            <!-- Tags -->
+                            <?php gsmtc_tags(); ?>
+						</div>
+					</div>
+				</div><!-- row -->
+			</div><!-- card -->
+			<?php endwhile; 
 
 	the_posts_navigation();
 
@@ -43,8 +70,10 @@ else :
 endif;
 ?>
 
-</main><!-- #main -->
-
+			</main><!-- #main -->
+		</div><!-- col -->
+		<?php get_sidebar(); ?>
+	</div><!-- row -->
+</div><!-- #content -->
 <?php
-get_sidebar();
 get_footer();
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -7,6 +7,192 @@
  * @package gesimatica
  */
 
+// Category Badge
+if ( ! function_exists( 'gsmtc_category_badge' ) ) :
+	function gsmtc_category_badge() {
+		// Hide category and tag text for pages.
+		if ( 'post' === get_post_type() ) {
+            echo '<div class="category-badge mb-2">';
+            $thelist = '';
+			$i = 0;
+            foreach( get_the_category() as $category ) {
+		      if ( 0 < $i ) $thelist .= ' ';
+						    $thelist .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" class="badge bg-secondary">' . $category->name.'</a>';
+						    $i++;
+            }
+            echo $thelist;	
+            echo '</div>';
+		}
+	}
+endif;
+// Category Badge End
+
+// Category
+if ( ! function_exists( 'gsmtc_category' ) ) :
+	function gsmtc_category() {
+		// Hide category and tag text for pages.
+		if ( 'post' === get_post_type() ) {
+			/* translators: used between list items, there is a space after the comma */
+			$categories_list = get_the_category_list( esc_html__( ', ', 'gsmtc' ) );
+			if ( $categories_list ) {
+				/* translators: 1: list of categories. */
+				printf( '<span class="cat-links"></span>', $categories_list ); // WPCS: XSS OK.	
+			}		
+		}
+	}
+endif;
+// Category End
+
+// Date
+if ( ! function_exists( 'gsmtc_date' ) ) :
+	/**
+	 * Prints HTML with meta information for the current post-date/time.
+	 */
+	function gsmtc_date() {
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time> <span class="time-updated-separator">/</span> <time class="updated" datetime="%3$s">%4$s</time>';
+		}
+
+		$time_string = sprintf( $time_string,
+			esc_attr( get_the_date( DATE_W3C ) ),
+			esc_html( get_the_date() ),
+			esc_attr( get_the_modified_date( DATE_W3C ) ),
+			esc_html( get_the_modified_date() )
+		);
+
+		$posted_on = sprintf(
+			/* translators: %s: post date. */
+			'%s',
+			'<span rel="bookmark">' . $time_string . '</span>'
+		);
+
+		echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
+
+	}
+endif;
+// Date End
+
+// Author
+if ( ! function_exists( 'gsmtc_author' ) ) :
+
+	function gsmtc_author() {
+		$byline = sprintf(
+			esc_html_x( 'by %s', 'post author', 'bootscore' ),
+			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		);
+		echo '<span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	}
+endif;
+// Author End
+
+// Comments
+if ( ! function_exists( 'gsmtc_comments' ) ) :
+	/**
+	 * Prints HTML with meta information for the categories, tags and comments.
+	 */
+	function gsmtc_comments() {
+
+
+		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+			echo ' | <i class="far fa-comments"></i> <span class="comments-link">';
+			comments_popup_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: post title */
+						__( 'Leave a Comment', 'bootscore' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				)
+			);
+			echo '</span>';
+		}
+	}
+endif;
+// Comments End
+
+// Edit Link
+if ( ! function_exists( 'gsmtc_edit' ) ) :
+	/**
+	 * Prints HTML with the comment count for the current post.
+	 */
+	function gsmtc_edit() {
+
+		edit_post_link(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Edit', 'bootscore' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			),
+			' | <span class="edit-link">',
+			'</span>'
+		);
+	}
+endif;
+// Edit Link End
+
+// Single Comments Count
+if ( ! function_exists( 'gsmtc_comment_count' ) ) :
+	/**
+	 * Prints HTML with the comment count for the current post.
+	 */
+	function gsmtc_comment_count() {
+		if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+			echo ' | <i class="far fa-comments"></i> <span class="comments-link">';
+
+			/* translators: %s: Name of current post. Only visible to screen readers. */
+			// comments_popup_link( sprintf( __( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'bootscore' ), get_the_title() ) );
+			comments_popup_link( sprintf( __( 'Leave a comment', 'bootscore' ), get_the_title() ) );
+
+
+			echo '</span>';
+		}
+	}
+endif;
+// Single Comments Count End
+
+// Tags
+if ( ! function_exists( 'gsmtc_tags' ) ) :
+	/**
+	 * Prints HTML with meta information for the categories, tags and comments.
+	 */
+	function gsmtc_tags() {
+		// Hide category and tag text for pages.
+		if ( 'post' === get_post_type() ) {
+
+
+			/* translators: used between list items, there is a space after the comma */
+			$tags_list = get_the_tag_list( '', ' ' );
+			if ( $tags_list ) {
+				/* translators: 1: list of tags. */
+				printf( '<div class="tags-links mt-2">' . esc_html__( 'Tagged %1$s', 'bootscore' ) . '</div>', $tags_list ); // WPCS: XSS OK.
+			}
+		}
+	}
+endif;
+
+
+add_filter( "term_links-post_tag", 'add_tag_class');
+
+//function add_tag_class($links) {
+//    return str_replace('<a href="', '<a class="badge bg-secondary" href="', $links);
+//}
+// Tags End
+
+
+
 if ( ! function_exists( 'gsmtc_posted_on' ) ) :
 	/**
 	 * Prints HTML with meta information for the current post-date/time.
